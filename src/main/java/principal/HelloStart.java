a2fc5dcba2e100efed5efe789b69775a3bdbf543
Lucero Raul <luceroraul12@gmail.com>
2022-11-23 17:56:23 -0300

    add classes exmaple

---
+++ src/main/java/principal/HelloStart.java
@@ -0,0 +1,9 @@
+package principal;
+
+public class HelloStart {
+    private String name;
+
+    public String getName() {
+        return this.name;
+    }
+}
---
+++ src/main/java/principal/HelloWorld.java
@@ -0,0 +1,9 @@
+package principal;
+
+public class HelloWorld {
+    private static String name = "HelloWorld.class";
+
+    public static String getName() {
+        return name;
+    }
+}
